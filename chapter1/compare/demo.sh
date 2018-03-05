590b579a5d7d5aeec09d1e1eb807f9fcb37b0860
castle <castleliu40@gmail.com>
2018-03-05 14:20:42 +0800

    add test example

---
+++ chapter1/compare/demo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# 展示比较与测试的shell语句
+
+# if 语句
+if condition; then
+    #statements
+else if condition; then
+    #statements
+else
+fi
+
+# 技巧，[condition] && action,如果condition为真，则执行action
+# 技巧，[condition] || action,如果condition为假，则执行action
+# 因选项过于繁杂，懒得手打－＿－
+
+
+
